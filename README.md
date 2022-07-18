f24ba4dbaeb568caa1f9dd0214fcc2dc92501080
longwei <1786082175@qq.com>
2022-07-18 22:10:40 +0800

    first commit

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# 云开发 quickstart
+# 吃什么随机
 
-这是云开发的快速启动指引，其中演示了如何上手使用云开发的三大基础能力：
+一款解决不知道吃什么的微信小程序
 
-- 数据库：一个既可在小程序前端操作，也能在云函数中读写的 JSON 文档型数据库
-- 文件存储：在小程序前端直接上传/下载云端文件，在云开发控制台可视化管理
-- 云函数：在云端运行的代码，微信私有协议天然鉴权，开发者只需编写业务逻辑代码
+# 主界面
+![架构图](./images/main_interface.jpg)
 
-## 参考文档
-
-- [云开发文档](https://developers.weixin.qq.com/miniprogram/dev/wxcloud/basis/getting-started.html)
+# 体验地址
 
--- miniprogram/pages/chishenme/index.js
+++ miniprogram/pages/chishenme/index.js
@@ -8,17 +8,36 @@ Page({
       changeBtn: false,
       foodName: '',
       timer: null,
+      randomTextList: [],
+      visible: false,
+      textAreaValueList: ['馄饨', '拉面', '烩面', '热干面', '刀削面', '油泼面', '炸酱面', '火锅','北京烤鸭','兰州拉面','四川串串香','重庆酸辣粉','武汉热干面','西安肉夹馍','长沙小龙虾','广东肠粉','小笼包','广西桂林米粉','柳州螺蛳粉','天津煎饼果子'],
+      textAreaValue: '',
+    },
+
+    onLoad() {
+        this.setData({
+            textAreaValue: this.data.textAreaValueList.join('，')
+        })
     },
 
     start() {
-        // wx.showLoading({
-        //     title: '别选了',
-        //   });
         const timer = setInterval(() => {
-            const list = ['馄饨', '拉面', '烩面', '热干面', '刀削面', '油泼面', '炸酱面', '火锅','北京烤鸭','兰州拉面','四川串串香','重庆酸辣粉','武汉热干面','西安肉夹馍','长沙小龙虾','广东肠粉','小笼包','广西桂林米粉','柳州螺蛳粉','天津煎饼果子','山东煎饼','东北锅包肉','福建佛跳墙','河南烩面','山西刀削面','南昌瓦罐汤','河北驴肉火烧','杨文婵', '刘晓媛'];
+            const list = this.data.textAreaValueList;
             const r = Math.ceil(Math.random() * list.length);
+            const foodName = list[r - 1];
+            let randomTextList = [];
+            for(let i = 0; i < 10; i++) {
+                const r = Math.ceil(Math.random() * list.length);
+                const foodName = list[r - 1];
+                const rTop = Math.ceil(Math.random() * wx.getSystemInfoSync().windowHeight);
+                const rLeft = Math.ceil(Math.random() * (wx.getSystemInfoSync().windowWidth - 50));
+                const rSize = Math.ceil(Math.random() * (37 - 14) + 14);
+                const rOpacity = Math.random();
+                randomTextList.push({ top: rTop, left: rLeft, fontSize: rSize, opacity: rOpacity, foodName});
+            }
             this.setData({
-                foodName: list[r - 1]
+                foodName,
+                randomTextList,
             })
         }, 100);
         this.setData({
@@ -26,20 +45,35 @@ Page({
             changeBtn: false,
             timer
           });
-
-  
-          const rTop = Math.ceil(Math.random() * wx.getSystemInfoSync().windowHeight);
-          const rLeft = Math.ceil(Math.random() * (wx.getSystemInfoSync().windowWidth - 50));
-          const rSize = Math.ceil(Math.random() * (37 - 14) + 14);
-          console.log(rTop, rLeft, rSize);
       },
 
       stop() {
-        clearInterval(this.data.timer)
+        clearInterval(this.data.timer);
         this.setData({
             showBtn: false,
             changeBtn: true,
+            timer: '',
           });
+      },
+
+      // 自定义食物名称
+      onOk() {
+          clearInterval(this.data.timer)
+          this.setData({
+            showBtn: false,
+            changeBtn: false,
+            timer: '',
+            foodName: '',
+            visible: !this.data.visible,
+            textAreaValue: this.data.textAreaValue,
+            textAreaValueList: this.data.textAreaValue.split('，')
+          });
+      },
+
+      onModal() {
+          this.setData({
+            visible: !this.data.visible,
+          })
       }
   });
   
